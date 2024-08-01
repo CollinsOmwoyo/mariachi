0ae8c9ad68b943e7a17fc1dc931033b0ebf02b00
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-08-01 05:03:23 +0300

    "Added 9 new function declarations to main.h"

--- main.h
+++ main.h
@@ -9,5 +9,15 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_string(char *str);
 int _print_int(int n);
+int _print_unsigned(unsigned int n);
+int _print_oct(unsigned int n);
+int _print_hex(unsigned int n);
+int _print_HEX(unsigned int n);
+int _print_pointer(void *p);
+int _print_exc_string(char *str);
+int _print_revs(char *str);
+int _print_rot13(char *str);
+int _print_bin(unsigned int n);
 
 #endif
+
